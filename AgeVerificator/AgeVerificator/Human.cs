8c789a6d335821eb42ca0f5e4a0797710aa7d0df
Jadamka <59932297+Jadamka@users.noreply.github.com>
2022-04-20 21:25:22 +0200

    Add files via upload

---
+++ AgeVerificator/AgeVerificator/Human.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeVerificator
+{
+    class Human
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public bool Adult { get; private set; }
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                age = value;
+                if(age < 18)
+                {
+                    Adult = false;
+                }
+                else
+                {
+                    Adult = true;
+                }
+            }
+        }
+
+        public Human(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Jmeno: {Name}\nPrijmeni: {Surname}\nAge: {age}\nDospely: {Adult}");
+        }
+    }
+}
---
+++ AgeVerificator/AgeVerificator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeVerificator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h1 = new Human("Franta", "Random", 18);
+
+            h1.Info();
+
+            Console.ReadKey();
+        }
+    }
+}
